e1fafc02d58bfbe64dc3125c50d83641848bbf0d
Ram0n18B <147212699+Ram0n18B@users.noreply.github.com>
2026-03-10 00:44:44 -0400

    Módulo 3

--- exercises/03_arrays/07_memcpy.c
+++ exercises/03_arrays/07_memcpy.c
@@ -12,7 +12,7 @@ int main() {
     // Debes calcular el tamaño total en bytes de forma dinámica usando 'sizeof'.
     
     // <--- ESCRIBE TU LLAMADA A memcpy AQUÍ ABAJO --->
-    
+    memcpy(backup_storage,sensor_data,sizeof(backup_storage)*sizeof(char));
 
     // --- ZONA DE VALIDACIÓN (No modificar) ---
     if (backup_storage[0] == 1.5 && backup_storage[4] == 5.9) {
